5d44a4a4293d2157d80bf60e47cba534a9ed1486
BrownCow371 <jmbrowncow@gmail.com>
2019-02-09 11:51:36 -0800

    completed lesson

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,39 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// iterative solution - runtim O(n)
+// function fib(n) {
+//    let fibArr = [0, 1];
+
+//    for(let i=2; i<=n; i++){
+//        fibArr[i]=(fibArr[i-2]+fibArr[i-1])
+//    }
+//     return fibArr[n];
+   
+// }
+
+// recursive solution - runtime O(2**n)
+
+function memoize(fn){
+    const cache = {};
+
+    return function(...args) {
+        if(cache[args]){
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+function fib(n) {
+    if(n<=1){
+        return n;
+    }
+    return fib(n-2)+fib(n-1)
+}
+   
+fib = memoize(fib);
 
 module.exports = fib;
