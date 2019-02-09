afcda3be6afc4a6a22bffd59fe1f455c31edcc69
BrownCow371 <jmbrowncow@gmail.com>
2019-02-09 11:53:24 -0800

    completed lesson

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((reversed, character)=>{
+        return character + reversed;
+    }, '');
+    
+}
+
+reverse('Fred');
+
+// alternative
+    // return str.split('').reverse().join('');
+
+// for of loop
+    // let reversed = '';{
+    // for(let character of str){
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+
+// my original 
+    // let array = str.split('');
+    // let revArray = [];
+    // for(let i = (array.length -1); i >= 0; i--){
+    //     revArray.push(array[i])
+    // };
+
+    // let revString = revArray.join('');
+    // return revString;
 
 module.exports = reverse;
