3e80e9641ad92fadb29b233c71c7cb3ed6374858
BrownCow371 <jmbrowncow@gmail.com>
2019-02-09 11:58:39 -0800

    completed lesson

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let counter = [0];
+    let store = [root, "s"];
+
+    while(store.length>1){
+        let node = store.shift();
+        
+        if(node === 's'){
+            store.push(node);
+            counter.push(0);
+        } else {    
+            store.push(...node.children);
+            counter[counter.length-1] += 1;
+        }
+    }
+    return counter;
+}
 
 module.exports = levelWidth;
