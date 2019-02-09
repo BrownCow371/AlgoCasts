3ad23d1b0cf05fad2ede1a68b330c35b6db4871d
BrownCow371 <jmbrowncow@gmail.com>
2019-02-09 11:54:18 -0800

    completed lesson

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     let space = " ";
+//     let arr = space.repeat(n).split('');
+//     for(let i=0; i<n; i++){
+//         arr[i]="#";
+//         console.log(arr.join(''))
+//     } 
+// }
+
+// function steps(n) {
+    
+//     for(let row=0; row<n; row++){
+//         let stair = '';
+//         for(let col=0; col<n; col++){
+//             if(col<=row){
+//                 stair += "#";
+//             } else {
+//                 stair += " ";
+//             }
+//         }
+//         console.log(stair);
+//     } 
+// }
+
+function steps(n, row=0, stair="") {
+    if(n===row){
+        return;
+    } 
+    if(n===stair.length){
+        console.log(stair)
+        return steps(n, ++row);
+    }
+    if(stair.length<=row){
+        stair += "#";
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+
+}
 
 module.exports = steps;
