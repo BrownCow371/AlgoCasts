098b96dd7cf9937f870c5b8eb3d7d4fdfe319443
BrownCow371 <jmbrowncow@gmail.com>
2019-02-09 11:54:41 -0800

    completed lesson

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars ={};
+    for(let letter of str){
+        chars[letter] ? chars[letter]++ : chars[letter] = 1 
+    }
+    
+    let maxChar;
+    let maxVal = 0;
+
+    Object.keys(chars).forEach(key => {
+        if (chars[key] > maxVal) {
+            maxVal=chars[key];
+            maxChar = key;
+        }
+    })
+    return maxChar;
+
+}
 
 module.exports = maxChar;
