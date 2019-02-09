407f1e91690728f044499ff6d5b5239826a5f32b
BrownCow371 <jmbrowncow@gmail.com>
2019-02-09 11:52:04 -0800

    completed lesson

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+
+    function fb(num) {
+        if (num % 15 === 0){
+            return "fizzbuzz";
+        } else if (num % 3 === 0){ 
+            return "fizz";
+        } else if (num % 5 === 0){
+            return "buzz";
+        } else {
+        return num;
+        }
+    }
+
+    for(let i=1; i<=n; i++){
+        console.log(fb(i))
+    }
+}
 
 module.exports = fizzBuzz;
+
